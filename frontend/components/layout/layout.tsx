7148116b7cd4fd7666ee661f3f35e5a0f8b57caa
jeremiahbrem <jeremiah.brem@gmail.com>
2023-07-31 22:56:16 -0500

    infrastructure

--- frontend/components/layout/layout.tsx
+++ frontend/components/layout/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +10,12 @@ export default function Layout({
 }) {
   return (
     <div className={inter.className}>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      </Head>
       {children}
     </div>
   )
--- infrastructure/main.go
+++ infrastructure/main.go
@@ -2,80 +2,106 @@ package main
 
 import (
 	"github.com/pulumi/pulumi-docker/sdk/v4/go/docker"
+	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/artifactregistry"
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/cloudrun"
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/projects"
+	"github.com/pulumi/pulumi-random/sdk/v4/go/random"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
+	projectId := "quiet-platform-392619"
+	dnsName := "homepersonaltrainers.net"
+
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		enableResourceService, err := projects.NewService(ctx, "EnableResourceService", &projects.ServiceArgs{
+		enableResourceService, _ := projects.NewService(ctx, "EnableResourceService", &projects.ServiceArgs{
 			DisableDependentServices: pulumi.Bool(true),
-			Project:                  pulumi.String("quiet-platform-392619"),
+			Project:                  pulumi.String(projectId),
 			Service:                  pulumi.String("cloudresourcemanager.googleapis.com"),
 		})
 
-		if err != nil {
-			return err
-		}
-
-		enableCloudRun, err := projects.NewService(ctx, "EnableCloudRun", &projects.ServiceArgs{
+		enableCloudRun, _ := projects.NewService(ctx, "EnableCloudRun", &projects.ServiceArgs{
 			DisableDependentServices: pulumi.Bool(true),
 			Project:                  pulumi.String("quiet-platform-392619"),
 			Service:                  pulumi.String("run.googleapis.com"),
 		}, pulumi.DependsOn([]pulumi.Resource{enableResourceService}))
 
-		if err != nil {
-			return err
-		}
-
-		enableCloudDns, err := projects.NewService(ctx, "EnableCloudDns", &projects.ServiceArgs{
-			DisableDependentServices: pulumi.Bool(true),
-			Project:                  pulumi.String("quiet-platform-392619"),
-			Service:                  pulumi.String("run.googleapis.com"),
-		}, pulumi.DependsOn([]pulumi.Resource{enableCloudRun}))
-
-		if err != nil {
-			return err
-		}
-
 		location := "us-central1"
 
-		// 		cmd := exec.Command("git rev-parse", "-");
-
-		// // The `Output` method executes the command and
-		// // collects the output, returning its value
-		// out, err := cmd.Output()
-		// if err != nil {
-		//   // if there was any error, print it here
-		//   fmt.Println("could not run command: ", err)
-		// }
-		// // otherwise, print the output from running the command
-		// fmt.Println("Output: ", string(out))
-
-		frontendImage, err := docker.NewImage(ctx, "frontend", &docker.ImageArgs{
-			Build: &docker.DockerBuildArgs{
-				Context:    pulumi.String("."),
-				Dockerfile: pulumi.String("Dockerfile"),
-			},
-			ImageName: pulumi.String("username/image:tag1"),
-			SkipPush:  pulumi.Bool(true),
+		uniqueString, _ := random.NewRandomString(ctx, "unique-string", &random.RandomStringArgs{
+			Length:  pulumi.Int(4),
+			Lower:   pulumi.Bool(true),
+			Upper:   pulumi.Bool(false),
+			Numeric: pulumi.Bool(true),
+			Special: pulumi.Bool(false),
 		})
 
-		if err != nil {
-			return err
-		}
+		repoId := pulumi.Sprintf("repo-%s", uniqueString.Result)
 
-		frontend, err := cloudrun.NewService(ctx, "frontend", &cloudrun.ServiceArgs{
+		repository, _ := artifactregistry.NewRepository(ctx, "repository", &artifactregistry.RepositoryArgs{
+			Description:  pulumi.String("Repository for container image"),
+			Format:       pulumi.String("DOCKER"),
+			Location:     pulumi.String(location),
+			RepositoryId: repoId,
+		})
+
+		repoUrl := pulumi.Sprintf("%s-docker.pkg.dev/%s/%s", repository.Location, projectId, repository.RepositoryId)
+
+		var backendImageName = "backend:latest"
+
+		backend, _ := docker.NewImage(ctx, "backend", &docker.ImageArgs{
+			Registry:  docker.RegistryArgs{},
+			ImageName: pulumi.Sprintf("%s/%s", repoUrl, backendImageName),
+			Build: &docker.DockerBuildArgs{
+				Context:  pulumi.String("../backend"),
+				Platform: pulumi.String("linux/amd64"),
+			},
+		})
+
+		backendService, _ := cloudrun.NewService(ctx, "backend-service", &cloudrun.ServiceArgs{
 			Location: pulumi.String(location),
 			Metadata: &cloudrun.ServiceMetadataArgs{
-				Namespace: pulumi.String("home-personal-training"),
+				Namespace: pulumi.String(projectId),
 			},
 			Template: &cloudrun.ServiceTemplateArgs{
 				Spec: &cloudrun.ServiceTemplateSpecArgs{
 					Containers: cloudrun.ServiceTemplateSpecContainerArray{
 						&cloudrun.ServiceTemplateSpecContainerArgs{
-							Image: pulumi.String("us-docker.pkg.dev/cloudrun/container/hello"),
+							Image: backend.ImageName,
+						},
+					},
+				},
+			},
+		}, pulumi.DependsOn([]pulumi.Resource{enableCloudRun}))
+
+		cloudrun.NewIamMember(ctx, "backend-iam", &cloudrun.IamMemberArgs{
+			Service:  backendService.Name,
+			Location: pulumi.String(location),
+			Member:   pulumi.String("allUsers"),
+			Role:     pulumi.String("roles/run.invoker"),
+		})
+
+		var frontendImageName = "frontend:latest"
+
+		frontend, _ := docker.NewImage(ctx, "frontend", &docker.ImageArgs{
+			Registry:  docker.RegistryArgs{},
+			ImageName: pulumi.Sprintf("%s/%s", repoUrl, frontendImageName),
+			Build: &docker.DockerBuildArgs{
+				Context:  pulumi.String("../frontend"),
+				Platform: pulumi.String("linux/amd64"),
+			},
+		})
+
+		frontendService, _ := cloudrun.NewService(ctx, "frontend-service", &cloudrun.ServiceArgs{
+			Location: pulumi.String(location),
+			Metadata: &cloudrun.ServiceMetadataArgs{
+				Namespace: pulumi.String(projectId),
+			},
+			Template: &cloudrun.ServiceTemplateArgs{
+				Spec: &cloudrun.ServiceTemplateSpecArgs{
+					Containers: cloudrun.ServiceTemplateSpecContainerArray{
+						&cloudrun.ServiceTemplateSpecContainerArgs{
+							Image: frontend.ImageName,
 						},
 					},
 				},
@@ -86,22 +112,28 @@ func main() {
 					LatestRevision: pulumi.Bool(true),
 				},
 			},
+		}, pulumi.DependsOn([]pulumi.Resource{enableCloudRun}))
+
+		cloudrun.NewIamMember(ctx, "frontend-iam", &cloudrun.IamMemberArgs{
+			Service:  frontendService.Name,
+			Location: pulumi.String(location),
+			Member:   pulumi.String("allUsers"),
+			Role:     pulumi.String("roles/run.invoker"),
 		})
-		if err != nil {
-			return err
-		}
-		_, err = cloudrun.NewDomainMapping(ctx, "defaultDomainMapping", &cloudrun.DomainMappingArgs{
-			Location: pulumi.String("us-central1"),
+
+		cloudrun.NewDomainMapping(ctx, "domain-mapping", &cloudrun.DomainMappingArgs{
+			Location: pulumi.String(location),
 			Metadata: &cloudrun.DomainMappingMetadataArgs{
-				Namespace: pulumi.String("home-personal-training"),
+				Namespace: pulumi.String(projectId),
 			},
 			Spec: &cloudrun.DomainMappingSpecArgs{
-				RouteName: frontend.Name,
+				RouteName: frontendService.Name,
 			},
+			Name: pulumi.String(dnsName),
 		})
-		if err != nil {
-			return err
-		}
+
 		return nil
 	})
 }
+
+// gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://us-central1-docker.pkg.dev
