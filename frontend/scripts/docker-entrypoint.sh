acfdfab5a98d6c39e550a43d9c8f61f3bfae22f9
jeremiahbrem <jeremiah.brem@gmail.com>
2023-08-02 22:07:02 -0500

    wait for it

--- frontend/scripts/docker-entrypoint.sh
+++ frontend/scripts/docker-entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-npm run build
-npm run start
+set -e
+
+# Wait for the backend to be up, if we know where it is.
+if [ -n "$BACKEND_HOST" ]; then
+  /usr/src/app/wait-for-it.sh "$BACKEND_HOST:${BACKEND_PORT:-8080}"
+fi
+
+# Run the main container command.
+exec "$@"
