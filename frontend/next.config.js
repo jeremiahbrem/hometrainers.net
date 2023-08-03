8d0a12871ce1bee0ef3945871ccc350b623b426d
jeremiahbrem <jeremiah.brem@gmail.com>
2023-08-03 12:42:47 -0500

    networks

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const API_URL = process.env.NODE_ENV === 'production'
-  ? 'http://backend:8080'
-  : 'http://localhost:8080'
-
 const nextConfig = {
-  async rewrites() {
-		return [
-			{
-				source: '/api/:path*',
-				destination: `${API_URL}/:path*`,
-			},
-		]
-	},
+  env: {
+    api: process.env.NODE_ENV === 'production'
+      ? 'http://backend:8080'
+      : 'http://localhost:8080'
+  }
 }
 
 module.exports = nextConfig
