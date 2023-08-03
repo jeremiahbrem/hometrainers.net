fbbeec82e7d2a01c83ffbb91974ad726d58c116c
jeremiahbrem <jeremiah.brem@gmail.com>
2023-08-03 09:15:17 -0500

    try in script again

--- frontend/scripts/docker-entrypoint.sh
+++ frontend/scripts/docker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+RETRIES=0
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' ${BACKEND_URL})" != "200" ]]
+  || [[ $RETRIES -LT 10 ]]; do sleep 5; ((RETRIES++)); done
+
 npm run build
 
 npm run start
