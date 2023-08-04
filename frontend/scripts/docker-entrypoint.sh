644239b7e7f214aa6c5ed32b03126c275bd663d8
jeremiahbrem <jeremiah.brem@gmail.com>
2023-08-04 08:33:28 -0500

    just wait

--- frontend/scripts/docker-entrypoint.sh
+++ frontend/scripts/docker-entrypoint.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-attempts=0
-status="500"
-url='http://localhost:8080'
+# attempts=0
+# status="500"
+# url='http://localhost:8080'
 
-while [[ $status -ne "200" && $attempts -ne 12 ]];
-  do
-    attempts=$((attempts+1))
-    echo "trying api ready check..."
-    echo "attempt: $attempts"
+# while [[ $status -ne "200" && $attempts -ne 12 ]];
+#   do
+#     attempts=$((attempts+1))
+#     echo "trying api ready check..."
+#     echo "attempt: $attempts"
 
-    status="$(curl -s -o /dev/null -w "%{http_code}" $url)"
-    echo "$status"
+#     status="$(curl -s -o /dev/null -w "%{http_code}" $url)"
+#     echo "$status"
 
-    sleep 5
-done
+#     sleep 5
+# done
 
-if [[ $status -ne "200" && $attempts -eq 12 ]];
-  then
-    echo "api not available"
-    echo 11
-  else
-    npm run build
-    npm run start
-fi
+# if [[ $status -ne "200" && $attempts -eq 12 ]];
+#   then
+#     echo "api not available"
+#     echo 11
+#   else
+#     npm run build
+#     npm run start
+# fi
 
+sleep 60
+
+npm run build
+npm run start
