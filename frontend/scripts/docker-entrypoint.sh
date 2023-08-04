887cd94263834efd4ad5dbd454a2a3d766432b4d
jeremiahbrem <jeremiah.brem@gmail.com>
2023-08-03 22:20:38 -0500

    fix counter

--- frontend/scripts/docker-entrypoint.sh
+++ frontend/scripts/docker-entrypoint.sh
@@ -4,10 +4,10 @@ retries=0
 status="500"
 url='http://api:8080'
 
-while [[ $status -ne "200" && $retries -ne 3 ]];
+while [[ $status -ne "200" && $retries -ne 12 ]];
   do
     status="$(curl -s -o /dev/null -w ''%{http_code}'' $url)"
-    sleep 1
+    sleep 5
     retries=$((retries+1))
     echo "retrying api ready check..."
     echo "$retries"
