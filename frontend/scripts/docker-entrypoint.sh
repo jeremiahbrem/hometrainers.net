fccca64a230d383aaecf125300b9f2f0d1e65193
jeremiahbrem <jeremiah.brem@gmail.com>
2023-08-04 07:46:16 -0500

    0.0.0.0

--- frontend/scripts/docker-entrypoint.sh
+++ frontend/scripts/docker-entrypoint.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-retries=0
+attempts=0
 status="500"
-url='backend:8080'
+url='0.0.0.0:8080'
 
-while [[ $status -ne "200" && $retries -ne 12 ]];
+while [[ $status -ne "200" && $attempts -ne 12 ]];
   do
-    status="$(curl -s -o /dev/null -w ''%{http_code}'' $url)"
+    attempts=$((attempts+1))
+    echo "trying api ready check..."
+    echo "attempt: $attempts"
+
+    status="$(curl -s -o /dev/null -w "%{http_code}" $url)"
+    echo "$status"
+
     sleep 5
-    retries=$((retries+1))
-    echo "retrying api ready check..."
-    echo "$retries"
 done
 
-if [[ $status -ne "200" && $retries == 12 ]];
+if [[ $status -ne "200" && $attempts -eq 12 ]];
   then
     echo "api not available"
     echo 11
