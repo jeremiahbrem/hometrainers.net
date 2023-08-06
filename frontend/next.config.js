e1f14efc8e01ab644691112317325bd8242bb9d3
jeremiahbrem <jeremiah.brem@gmail.com>
2023-08-06 08:58:26 -0500

    remove env from next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    api: process.env.NEXT_PUBLIC_API_URL,
-    googleClientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
-    googleClientID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-  },
   experimental: {
     esmExternals: false
   },
