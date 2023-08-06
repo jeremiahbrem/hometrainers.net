46d20e7e63d2f8a6d7bbeac7963a1ad7c3b49ab3
jeremiahbrem <jeremiah.brem@gmail.com>
2023-08-06 01:41:05 -0500

    next env

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    api: process.env.API_URL
+    api: process.env.API_URL,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
   },
   experimental: {
     esmExternals: false
