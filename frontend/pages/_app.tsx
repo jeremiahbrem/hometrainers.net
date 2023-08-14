2c7a7879852cb50ee81c8d8c382aac3fc1c16641
jeremiahbrem <jeremiah.brem@gmail.com>
2023-08-13 20:33:55 -0500

    skip auth

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -7,8 +7,8 @@ export default function App({
   pageProps: { session, ...pageProps }
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
+    // <SessionProvider session={session}>
       <Component {...pageProps} />
-    </SessionProvider>
+    // </SessionProvider>
   )
 }
