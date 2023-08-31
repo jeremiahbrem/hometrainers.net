a08fab379c36bf4464e3334f3e7de360fa5ae3e5
jeremiahbrem <jeremiah.brem@gmail.com>
2023-08-30 23:45:58 -0500

    fix postgres db env

--- auth/database/database.go
+++ auth/database/database.go
@@ -23,7 +23,7 @@ func ConnectDb() {
 		os.Getenv("POSTGRES_HOST"),
 		os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("POSTGRES_NAME"),
+		os.Getenv("POSTGRES_DB"),
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
