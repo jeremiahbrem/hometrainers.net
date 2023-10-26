31c0ee108c0a1fd8156d81910272ea18c8f3c7f3
jeremiahbrem <jeremiah.brem@gmail.com>
2023-10-26 12:36:55 -0500

    header no home link

--- frontend/components/logo/index.tsx
+++ frontend/components/logo/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import styles from './logo.module.scss'
 import Link from 'next/link'
+import { useParams, usePathname } from 'next/navigation'
 
 type LogoProps = {
   className?: string
@@ -8,8 +9,11 @@ type LogoProps = {
 }
 
 export function Logo({ className, logo }: LogoProps) {
+  const path = usePathname()
+
+  const href = logo ? path : '/'
   return (
-    <Link href='/' className={[styles.logoContainer, className].filter(x => x).join(' ')}>
+    <Link href={href} className={[styles.logoContainer, className].filter(x => x).join(' ')}>
       <Image src={logo ?? '/hpt-logo.svg'} alt={'home personal trainer logo'} height={0} width={0} />
     </Link>
   )
