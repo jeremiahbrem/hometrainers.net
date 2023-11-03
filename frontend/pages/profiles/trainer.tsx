dea29872a5e6509c26d46efbefaacb3c299e4ad6
jeremiahbrem <jeremiah.brem@gmail.com>
2023-11-03 17:30:51 -0500

    gtag

--- frontend/pages/profiles/trainer.tsx
+++ frontend/pages/profiles/trainer.tsx
@@ -1,5 +1,6 @@
 import Layout from '@/components/layout'
 import { ProfilePage } from '@/components/profile-page'
+import Script from 'next/script'
 
 export default function TrainerProfile() {
   return (
@@ -7,6 +8,16 @@ export default function TrainerProfile() {
       title: 'HomeTrainers.net | Create Trainer Profile',
       description: 'Create a trainer profile'
     }}>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=AW-11216865483" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'AW-11216865483');
+        `}
+      </Script>
       <ProfilePage type='trainer' />
     </Layout>
   )
